a041cc83abd3ace8640e66845c1971d1b50adfb1
KritiKaushal5 <73942222+KritiKaushal5@users.noreply.github.com>
2020-11-04 18:16:22 +0530

    Add files via upload

---
+++ StudentRegistration/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddStudentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
